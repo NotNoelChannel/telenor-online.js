b00aa28d143bd902d6b0525174d831fa169bede5
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-10-19 20:48:40 +0200

    hmm

--- telenor-online.js
+++ telenor-online.js
@@ -1,10 +1,10 @@
 // This script creates a cookie if the user clicks on the login button. No framework or backend used. Made by Szeccsa.
-function setCookie(name, value, days) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+function setCookie(name, value, days) { // this is a function that defines the cookie's name, value and days
+    const expires = new Date(Date.now() + days * 864e5).toUTCString(); // this defines the cookie's expire date. The current date + the day and it converts to UTC string.
+    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`; //sets the cookie 
 }
 
-function deleteCookie(name) {
+function deleteCookie(name) { // basically deletes the cookie
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
 }
 
